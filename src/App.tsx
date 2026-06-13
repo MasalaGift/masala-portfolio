7e3df117ecfbbc86e2cf55f5873bf0256b971a55
Gift Ratshili <giftratshili@gmail.com>
2026-06-13 14:59:30 +0200

    feat: add React Native Restaurant App project
    - Replace/update audio-recorder with full restaurant app
    - Include Expo, Firebase, Stripe, Redux Toolkit tech stack
    - Add live demo link (EAS build) and GitHub repo
    - Note: screenshots to be added in /screenshots/restaurant/

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import ProjectDetails from "./pages/ProjectsPage";
+import ProjectDetails from "../src/components/ProjectDetails";
 import ProjectsPage from "./pages/ProjectsPage";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/project/:id" element={<ProjectDetails />} />
-      <Route path="/projects" element={<ProjectsPage />} />
-    </Routes>
+   return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project/:id" element={<ProjectDetails />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
